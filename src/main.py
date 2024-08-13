f73784c38a7eea751a3b940a5ed0937e781c7a12
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-08-13 11:02:02 -0300

    feat: gets the eth & btc price

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+endpoints = {
+    "btc": 'https://coinmarketcap.com/es/currencies/bitcoin/',
+    "eth": 'https://coinmarketcap.com/es/currencies/ethereum/'
+}
+
+for currency, endpoint in endpoints.items():
+    response = requests.get(endpoint)
+
+    page = BeautifulSoup(response.text, 'html.parser')
+
+    element = page.find(class_="sc-65e7f566-0 clvjgF base-text")
+    price = element.get_text().split("$")[1]
+
+    print(f"{currency}: {price}")
