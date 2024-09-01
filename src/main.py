7b49e11ac4c689f0bd27310fdf8f7af8b7865e41
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-09-01 10:13:16 -0300

    fix(config): add pre-commit config

--- src/main.py
+++ src/main.py
@@ -2,14 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 endpoints = {
-    "btc": 'https://coinmarketcap.com/es/currencies/bitcoin/',
-    "eth": 'https://coinmarketcap.com/es/currencies/ethereum/'
+    "btc": "https://coinmarketcap.com/es/currencies/bitcoin/",
+    "eth": "https://coinmarketcap.com/es/currencies/ethereum/",
 }
 
 for currency, endpoint in endpoints.items():
     response = requests.get(endpoint)
 
-    page = BeautifulSoup(response.text, 'html.parser')
+    page = BeautifulSoup(response.text, "html.parser")
 
     element = page.find(class_="sc-65e7f566-0 clvjgF base-text")
     price = element.get_text().split("$")[1]
