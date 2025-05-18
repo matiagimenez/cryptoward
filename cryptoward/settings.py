f4930d1fff9c4718951d6cf39cf069291e2f546f
Matias Gimenez <matiasgimenez.dev@gmail.com>
2025-05-18 13:20:56 -0300

    fix(test): add fixtures for kafka settings

--- cryptoward/settings.py
+++ cryptoward/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class ApplicationSettings(BaseSettings):
-    DATASOURCE_URL: str
+    DATASOURCE_URL: str = "https://coinmarketcap.com/currencies"
 
     KAFKA_HOST: str
     KAFKA_TOPIC: str
