511a3e1dbf1dd9079911e660711bcbbc532e7b5c
Matias Gimenez <matiasgimenez.dev@gmail.com>
2025-05-18 13:23:52 -0300

    fix(test): move fixtures to correct test module

--- tests/injections/conftest.py
+++ tests/injections/conftest.py
@@ -1,27 +1,8 @@
-from collections.abc import Generator
-
 import pytest
 
 from cryptoward.injections import Environment
-from cryptoward.settings import Settings
 
 
 @pytest.fixture(params=[Environment.PRODUCTION, Environment.TEST])
 def environment(request: pytest.FixtureRequest) -> Environment:
     return request.param  # type: ignore[no-any-return]
-
-
-@pytest.fixture(autouse=True)
-def _patch_kafka_topic() -> Generator[None, None, None]:
-    old_value = Settings.KAFKA_TOPIC
-    Settings.KAFKA_TOPIC = "test_topic"
-    yield
-    Settings.KAFKA_TOPIC = old_value
-
-
-@pytest.fixture(autouse=True)
-def _patch_kafka_host() -> Generator[None, None, None]:
-    old_value = Settings.KAFKA_HOST
-    Settings.KAFKA_HOST = "host"
-    yield
-    Settings.KAFKA_HOST = old_value
