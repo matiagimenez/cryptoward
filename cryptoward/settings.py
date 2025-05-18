8e9c73cfab609ee32bd856a69acdcc0f3427afa8
Matias Gimenez <matiasgimenez.dev@gmail.com>
2025-05-18 13:34:41 -0300

    fix(config): set default values for KAFKA_HOST and KAFKA_TOPIC

--- cryptoward/settings.py
+++ cryptoward/settings.py
@@ -4,8 +4,8 @@ from pydantic_settings import BaseSettings
 class ApplicationSettings(BaseSettings):
     DATASOURCE_URL: str = "https://coinmarketcap.com/currencies"
 
-    KAFKA_HOST: str
-    KAFKA_TOPIC: str
+    KAFKA_HOST: str = "localhost:29092"
+    KAFKA_TOPIC: str = "cryptoward-events"
 
 
 Settings = ApplicationSettings()
