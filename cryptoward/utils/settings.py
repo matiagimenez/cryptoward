e1d67ae49957cbe289b91d9550e1e10731172a36
Matias Gimenez <matiasgimenez.dev@gmail.com>
2025-11-28 21:22:05 -0300

    fix(config): update application settings

--- cryptoward/utils/settings.py
+++ cryptoward/utils/settings.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class ApplicationSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-    DATASOURCE_URL: str = "https://coinmarketcap.com/currencies"
+    DATASOURCE_URL: str
     TELEGRAM_BOT_TOKEN: str
 
     @property
