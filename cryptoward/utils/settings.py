21042414b0449159b66d7604a7ba7f38d6af8d9b
Matias Gimenez <matiasgimenez.dev@gmail.com>
2025-11-28 22:50:22 -0300

    chore(config): add default value

--- cryptoward/utils/settings.py
+++ cryptoward/utils/settings.py
@@ -3,8 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class ApplicationSettings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-    DATASOURCE_URL: str
-    TELEGRAM_BOT_TOKEN: str
+    DATASOURCE_URL: str = "https://coinmarketcap.com/currencies"
+    TELEGRAM_BOT_TOKEN: str = ""
     PRICE_ELEMENT_TAG: str = "span"
     PRICE_ELEMENT_ATTRIBUTES: dict[str, str] = {"data-test": "text-cdp-price-display"}
 
