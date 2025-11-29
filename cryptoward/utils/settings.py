cf8d8c602c1ca4d005152636d6d0a48c4a9106ca
Matias Gimenez <matiasgimenez.dev@gmail.com>
2025-11-29 01:55:21 +0000

    fix(config): fix several issues

--- cryptoward/utils/settings.py
+++ cryptoward/utils/settings.py
@@ -13,4 +13,4 @@ class ApplicationSettings(BaseSettings):
         return ["bitcoin", "ethereum", "solana", "bnb", "aave", "avalanche", "monero"]
 
 
-Settings = ApplicationSettings()  # type: ignore[call-arg]
+Settings = ApplicationSettings()
